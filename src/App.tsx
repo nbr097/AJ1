3e66471eb9f810124a7cfab54c17fa13460c68d7
nbr097 <nicho.brown2@gmail.com>
2025-12-08 15:57:15 +1000

    Removed Theme Switcher

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,21 @@ import { QuotePage } from './pages/Quote';
 import { TermsPage } from './pages/Terms';
 import { TipsPage } from './pages/Tips';
 import { StoragePage } from './pages/Storage';
-import { ThemeProvider } from './context/ThemeContext';
+
 
 function App() {
   return (
-    <ThemeProvider>
-      <Router>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/quote" element={<QuotePage />} />
-            <Route path="/terms" element={<TermsPage />} />
-            <Route path="/tips" element={<TipsPage />} />
-            <Route path="/storage" element={<StoragePage />} />
-          </Routes>
-        </Layout>
-      </Router>
-    </ThemeProvider>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/quote" element={<QuotePage />} />
+          <Route path="/terms" element={<TermsPage />} />
+          <Route path="/tips" element={<TipsPage />} />
+          <Route path="/storage" element={<StoragePage />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
 
